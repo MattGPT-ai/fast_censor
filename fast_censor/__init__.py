2e02333bc29de69aa4ee93724417dd4ef3aa4d7d
Matt Buchovecky <mbuchove@gmail.com>
2023-06-30 12:00:04 -0700

    adding fast_censor/__init__.py

---
+++ fast_censor/__init__.py
@@ -0,0 +1,17 @@
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__",
+    "__author__", "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "ProfanityTrie"
+__summary__ = "quickly filter profanity from text"
+__uri__ = ""
+
+__version__ = "0.1"
+__build__ = ""
+
+__author__ = "Matt Buchovecky (mbuchove)"
+__email__ = "mbuchove@gmail.com"
+
+__license__ = ""
+__copyright__ = "Copyright 2022"
