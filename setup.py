d24bc2f70ab1664b68f6805f4c136b3e577ed04a
Matt Buchovecky <mbuchove@gmail.com>
2023-06-30 21:02:00 -0700

    adding setup.py to install package

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="fast-censor",
+    version="0.1",
+    description="A fast and flexible utility for censoring and filtering text",
+    author="Matt Buchovecky",
+    author_email="mbuchove@gmail.com",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
