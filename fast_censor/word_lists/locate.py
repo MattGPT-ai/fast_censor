7b180ecaf96cf2cffe93cf917b89772105def69c
Matt Buchovecky <mbuchove@gmail.com>
2023-07-02 22:36:26 -0700

    add script for locating supplied .txt files

---
+++ fast_censor/word_lists/locate.py
@@ -0,0 +1,6 @@
+"""this file is here simply to give the directory containing word lists in a way that can be imported"""
+
+from os import path
+
+
+word_list_dir = path.abspath(path.dirname(__file__))
