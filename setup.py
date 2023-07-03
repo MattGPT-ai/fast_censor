23a851fedc6e7cbff04e55ee50ef2631128ed49e
Matt Buchovecky <mbuchove@gmail.com>
2023-07-02 21:24:03 -0700

    use README.md for long_description, update README

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ from setuptools import setup, find_packages
 setup(
     name="fast-censor",
     version="0.2",
-    description="A fast and flexible utility for censoring and filtering text",
     author="Matt Buchovecky",
     author_email="mbuchove@gmail.com",
+    description="A fast and flexible utility for censoring and filtering text",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown', 
     packages=find_packages(),
     package_data={
         "fast-censor": ["fast_censor/word_lists/*"],
