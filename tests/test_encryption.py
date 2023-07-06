d788e4895e6e5ecd4ed6fa568bddc4e4cf759c8b
Matt Buchovecky <mbuchove@gmail.com>
2023-07-06 00:55:57 -0700

    Completing docstrings in Google format, improving comments and a function name
    use locate module

--- tests/test_encryption.py
+++ tests/test_encryption.py
@@ -5,13 +5,14 @@ from fast_censor.wordlist_file_handler import WordListHandler
 
 
 class TestWordListHandler(TestCase):
+    """Tests functionality of the wordlist file handler"""
 
     def setUp(self):
-        """initializes word list handler"""
-        self.word_list_handler = WordListHandler()
+        """Initializes a default word list handler"""
+        self.word_list_handler = WordListHandler(debug=True)
 
     def test_encode_decode(self):
-        """ensure that if you encode and decode a string, you get back the original string"""
+        """Ensure that if you encode and decode a string, you get back the original string"""
         for string in ['hello', 'hi there', 'hi\nthere']:
             self.assertEqual(string, self.word_list_handler.decode_string(self.word_list_handler.encode_string(string)))
 
--- tests/test_filter.py
+++ tests/test_filter.py
@@ -1,4 +1,4 @@
-"""tests the primary functionality of the fast censor"""
+"""Tests the primary functionality of the fast censor"""
 
 from unittest import TestCase
 import unittest.main
@@ -9,10 +9,12 @@ from fast_censor.word_lists.locate import word_list_dir
 
 class TestFilter(TestCase):
     def setUp(self):
+        """Instantiate the FastCensor object with the decoded short clean wordlist for easier debugging"""
         self.profanity_filter = FastCensor(wordlist=f"{word_list_dir}/clean_wordlist_decoded.txt",
                                            wordlist_encoded=False)
 
     def test_filter_word(self):
+        """Check that the proper profanity matches are found within a string"""
         res = self.profanity_filter.check_text("there fvdge fudgey  ri1i1i1liick  f_u_d_g_e cow swirl saa@ax crap")
         self.assertEqual(res, [(6, 11), (12, 17), (20, 32)])
 
